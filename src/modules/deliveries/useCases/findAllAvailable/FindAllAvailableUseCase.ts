9d3e1d6b0ee236b506f16632cef3e70f3e2818e7
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-10 18:39:31 -0300

    feat: change where of FindAllAvailableUseCase

--- src/modules/deliveries/useCases/findAllAvailable/FindAllAvailableUseCase.ts
+++ src/modules/deliveries/useCases/findAllAvailable/FindAllAvailableUseCase.ts
@@ -4,6 +4,7 @@ export class FindAllAvailableUseCase {
   async execute() {
     const deliveries = await prisma.deliveries.findMany({
       where: {
+        deliveryman_id: null,
         end_at: null,
       },
     })
