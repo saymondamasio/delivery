681f19cec1ccbde85fc9480d10f775a2a6cd0627
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-10 11:41:47 -0300

    feat: authenticate client

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express'
+import { AuthenticateClientController } from './modules/accounts/useCases/authenticateClient/AuthenticateClientController'
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController'
 
 const routes = Router()
 
-routes.post('/clients/', new CreateClientController().handle)
+routes.post('/clients', new CreateClientController().handle)
+routes.post('/sessions', new AuthenticateClientController().handle)
 
 export { routes }
