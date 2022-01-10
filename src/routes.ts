c3f74d69928db8f288f939891baf453abab81269
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-10 10:18:10 -0300

    feat: create client

---
+++ src/routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController'
+
+const routes = Router()
+
+routes.post('/clients/', new CreateClientController().handle)
+
+export { routes }
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import express from 'express'
+import { routes } from './routes'
 
 const app = express()
 
-app.listen(3000, () => console.log('Server is running on port 3000 🚀🚀'))
+app.use(express.json())
+
+app.use(routes)
+
+app.listen(3333, () => console.log('Server is running on port 3333 🚀🚀'))
