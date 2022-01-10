57f716c0afe3475d07ed8f11c1562bae101870b8
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-10 12:16:07 -0300

    feat: create deliveryman

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import { AuthenticateClientController } from './modules/accounts/useCases/authenticateClient/AuthenticateClientController'
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController'
+import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController'
 
 const routes = Router()
 
 routes.post('/clients', new CreateClientController().handle)
+routes.post('/deliveryman', new CreateDeliverymanController().handle)
 routes.post('/sessions', new AuthenticateClientController().handle)
 
 export { routes }
